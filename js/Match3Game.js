ce42e6d320337a394eabbdb0dbb1070f91d9d0e9
ZHOUYUANN <464814413@qq.com>
2025-10-16 15:51:41 +0800

    取出图片预加载

--- js/Match3Game.js
+++ js/Match3Game.js
@@ -13,31 +13,31 @@ class Match3Game {
 	async start() {
 		// 预加载图片资源
 		try {
-			await this.state.preloadImages([
-				'img/bg-1.png',
-				'img/bgb-2.png',
-				'img/cao.png',
-				'img/ostrich.png',
-				'img/zebra.png',
-				'img/deer.png',
-				'img/elephant.png',
-				'img/lion.png',
-				'img/bear.png',
-				'img/buffalo.png',
-				'img/score.png',
-				'img/yeniu.png',
-				'img/skill.png',
-				'img/board.png',
-				'img/ostrich_icon.png',
-				'img/zebra_icon.png',
-				'img/deer_icon.png',
-				'img/elephant_icon.png',
-				'img/lion_icon.png',
-				'img/bear_icon.png',
-				'img/buffalo_icon.png',
-				'img/game_over.png',
-				'img/buffalo_warn.png'
-			])
+			// await this.state.preloadImages([
+			// 	'img/bg-1.png',
+			// 	'img/bgb-2.png',
+			// 	'img/cao.png',
+			// 	'img/ostrich.png',
+			// 	'img/zebra.png',
+			// 	'img/deer.png',
+			// 	'img/elephant.png',
+			// 	'img/lion.png',
+			// 	'img/bear.png',
+			// 	'img/buffalo.png',
+			// 	'img/score.png',
+			// 	'img/yeniu.png',
+			// 	'img/skill.png',
+			// 	'img/board.png',
+			// 	'img/ostrich_icon.png',
+			// 	'img/zebra_icon.png',
+			// 	'img/deer_icon.png',
+			// 	'img/elephant_icon.png',
+			// 	'img/lion_icon.png',
+			// 	'img/bear_icon.png',
+			// 	'img/buffalo_icon.png',
+			// 	'img/game_over.png',
+			// 	'img/buffalo_warn.png'
+			// ])
 			// 预加载音效
 			await this.soundManager.loadSounds({
 				background: 'sounds/background.mp3',
